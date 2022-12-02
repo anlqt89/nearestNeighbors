0b0b84bb70d27c9343bf130a739c775d0d68d9fe
An Lam <82059787+anlqt89@users.noreply.github.com>
2022-12-02 17:15:43 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -13,24 +13,10 @@ from ftfy import fix_text
 from sklearn.neighbors import NearestNeighbors
 from resume_parser import resumeparse
 import json
-# from types import MethodDescriptorType
-# import flask
-# from flask import jsonify, request
-# from flask import json
-# from flask_cors import CORS
-
-# app = flask.Flask(__name__)
-# app.config["DEBUG"] = True
-# CORS(app)
-
-# # http://127.0.0.1:5000/ai?fn=%22resume.csv%22
-# @app.route('/ai', methods=['GET'])
-
-# def ai(filename):
-#     return jsonify(run_ai(filename))
+from collections.abc import Iterable
 
 # app.run()
-
+# filename = "Resume- Thu Nguyen.docx"
 def run_ai(filename):
   stop_words  = set(stopwords.words('english'))
 
@@ -38,17 +24,19 @@ def run_ai(filename):
   df =pd.read_csv('jobList.csv') 
   df['test']=df['Job_Description'].apply(lambda x: ' '.join([word for word in str(x).split() if len(word)>2 and word not in (stop_words)]))
 
-
   #read resume
-  stop_words = set(stopwords.words('english'))
-  punctuations = string.punctuation
   # ---------------------------------------------
   # Parse resume:
 
   #combine skills
   data = resumeparse.read_file(filename)
+  datalist = list(data.values())
+
   skills=[]
-  skills.append(' '.join(word for word in data))
+  for i in range(3, len(datalist)):
+    if isinstance(datalist[i], Iterable):
+        for word in datalist[i]:
+          skills.append(word)
 
   #Begin AI
   def ngrams(string, n=3):
@@ -71,35 +59,48 @@ def run_ai(filename):
 
   #convert to 2D dimension
   vectorizer = TfidfVectorizer(min_df=1, analyzer=ngrams, lowercase=False)
-  tfidf = vectorizer.fit_transform(skills)
+  tfidf = vectorizer.fit_transform(skills) #document-term matrix.
+
+  # print(tfidf)
 
   #base line for skills
-  nbrs = NearestNeighbors(n_neighbors=1, n_jobs=-1).fit(tfidf)
-  test = (df['test'].values.astype('U'))
+  test1 = (df['test'].values.astype('U')) #Tes
 
-  #plot job description 
+  nbrs = NearestNeighbors(n_neighbors=1, n_jobs=-1).fit(tfidf) #plot the skills
+
+    #plot job description 
   def getNearestN(query):
     queryTFIDF_ = vectorizer.transform(query)
     distances, indices = nbrs.kneighbors(queryTFIDF_)
     return distances, indices
 
-  distances, indices = getNearestN(test) #distances: distance between skills and jobs_desc, indices: positions of jobs_desc
+  # print("---------------------------")
+  distances, indices = getNearestN(test1) #distances: distance between skills and jobs_desc, indices: positions of jobs_desc
+  # #round up the matching score
+  # test = list(test) 
 
-  #round up the matching score
-  test = list(test) 
+      
   matches = []
 
   for i,j in enumerate(indices):
-      dist=round(distances[i][0],2)
+      dist=round(distances[i][0],4)
       temp = [dist]
       matches.append(temp)
-      
+
   matches = pd.DataFrame(matches, columns=['Match confidence'])
+  # matches = pd.DataFrame(indices, columns=['Match confidence'])
+
   df['match']=matches['Match confidence']
+
   df1=df.sort_values('match')
-  df1 = df1[['match', 'url', 'Position', 'Company', 'Location']].head(10).reset_index()
-  
+
+  df1 = df1[['match', 'url', 'Position', 'Company', 'Location', 'Job_Description']]
+  df1 = df1[0:10]
   finFile = pd.DataFrame(df1)
-  res = json.dumps(finFile.to_dict('records'))
-  
-  return res
+  # finFile.to_json("top10.js")
+  finFile.to_csv("top10.csv")
+
+  f = open('top10.js')
+  rs = json.load(f)
+  return rs
+# print(rs)
